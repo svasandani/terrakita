6dad63cdc80c37cbbea066af4e1a6334201ef213
svasandani <vasandanishailesh@gmail.com>
2021-11-17 23:38:45 -0500

    Add filter models

--- internal/db/models.go
+++ internal/db/models.go
@@ -1,8 +1,32 @@
+package db
+
 /******** API Models ********/
 
-struct FilterRequest {
-  Lings []int `json:"lings"`
-  LingProperties []int `json:"ling_properties"`
-  Linglets []int `json:"linglets"`
-  LingletProperties []int `json:"linglet_properties"`
+/*=== Generic ===*/
+
+type PropertyValuePair struct {
+	Property string `json:"property"`
+	Value    string `json:"value"`
+}
+
+/*=== Filter ===*/
+
+type FilterRequest struct {
+	Lings             []int `json:"lings"`
+	LingProperties    []int `json:"ling_properties"`
+	Linglets          []int `json:"linglets"`
+	LingletProperties []int `json:"linglet_properties"`
+}
+
+type FilterResponse struct {
+	Lings []FilterResponseLing `json:"lings"`
+}
+
+type FilterResponseLing struct {
+	PropertyValuePairs []PropertyValuePair     `json:"property_value_pairs"`
+	Linglets           []FilterResponseLinglet `json:"linglets,omitempty"`
+}
+
+type FilterResponseLinglet struct {
+	PropertyValuePairs []PropertyValuePair `json:"property_value_pairs"`
 }
