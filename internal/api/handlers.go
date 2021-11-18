f7ceac08a64c7c4240c42cef049fab2ca1bd7ec5
hab278 <hab278@nyu.edu>
2021-11-18 00:04:34 -0500

    Add api handlers

---
+++ internal/api/handlers.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"net/http"
+  "encoding/json"
+  
+  "github.com/svasandani/terrakita/internal/db"
+)
+
+func PostSearchFilterHandler(w http.ResponseWriter, r *http.Request) {
+  var f db.FilterRequest
+  
+  err := json.NewDecoder(r.Body).Decode(&f)
+  if err != nil {
+      http.Error(w, err.Error(), http.StatusBadRequest)
+      return
+  }
+  
+  fr := db.FilterResponse{}
+  js, err := json.Marshal(fr)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  w.WriteHeader(http.StatusOK)
+  w.Write(js)
+}
