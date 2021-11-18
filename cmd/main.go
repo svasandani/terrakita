d8d1406602d828596cb4d426bfb20583aebd2af8
hab278 <hab278@nyu.edu>
2021-11-17 23:36:02 -0500

    We buggin out

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
   "fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("terrakita")
+  s := createBackendServer()
+	log.Fatal(s.ListenAndServe())
+}
+
+
+func createBackendServer() *http.Server {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+    fmt.Println(path)
+		w.Write([]byte(fmt.Sprintf("hello")))
+	})
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: mux,
+	}
+
+	return &server
 }
