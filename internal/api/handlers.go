9fa44b4bbe406c8808599fd41765450391288714
hab278 <hab278@nyu.edu>
2021-11-18 00:54:05 -0500

    Write error properly

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -11,7 +11,7 @@ func PostSearchFilterHandler(w http.ResponseWriter, r *http.Request) {
   var f db.FilterRequest
   var js []byte
   var er error
-  
+
   err := json.NewDecoder(r.Body).Decode(&f)
   if err != nil {
     js, er = errorResponse(err, http.StatusBadRequest)
@@ -19,8 +19,10 @@ func PostSearchFilterHandler(w http.ResponseWriter, r *http.Request) {
       http.Error(w, err.Error(), http.StatusInternalServerError)
       return
     }
+    writeResponse(w, js)
+    return
   }
-  
+
   fr, err := db.Filter(f)
   if err != nil {
     js, er = errorResponse(err, http.StatusInternalServerError)
@@ -28,8 +30,10 @@ func PostSearchFilterHandler(w http.ResponseWriter, r *http.Request) {
       http.Error(w, err.Error(), http.StatusInternalServerError)
       return
     }
+    writeResponse(w, js)
+    return
   }
-  
+
   js, err = json.Marshal(fr)
   if err != nil {
     js, er = errorResponse(err, http.StatusInternalServerError)
@@ -37,8 +41,14 @@ func PostSearchFilterHandler(w http.ResponseWriter, r *http.Request) {
       http.Error(w, err.Error(), http.StatusInternalServerError)
       return
     }
+    writeResponse(w, js)
+    return
   }
 
+  writeResponse(w, js)
+}
+
+func writeResponse(w http.ResponseWriter, js []byte) {
   w.Header().Set("Content-Type", "application/json")
   w.WriteHeader(http.StatusOK)
   w.Write(js)
