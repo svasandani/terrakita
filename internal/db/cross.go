442bfe067f69e86a2d3149e4ffc07e407b5fba64
svasandani <vasandanishailesh@gmail.com>
2021-11-23 19:50:19 -0500

    Add Linglet cross

--- internal/db/cross.go
+++ internal/db/cross.go
@@ -103,3 +103,98 @@ func CrossLingProperties(clpr CrossLingPropertiesRequest) (CrossLingPropertiesRe
 		PropertyCombinations: pc,
 	}, nil
 }
+
+func CrossLingletProperties(cllpr CrossLingletPropertiesRequest) (CrossLingletPropertiesResponse, error) {
+	err := validateCrossLingletPropertiesRequest(cllpr)
+	if err != nil {
+		log.Print("Malformed request!")
+		return CrossLingletPropertiesResponse{}, err
+	}
+
+	pnMap := make(map[string]int)
+	lMap := make(map[string][]NameValuePair)
+	pMap := make(map[[6]NameValuePair][]string)
+	properties := make([]string, len(cllpr.LingletProperties))
+
+	// pass group then ling properties into query args
+	qargs := make([]interface{}, len(cllpr.LingletProperties)+1)
+	qargs[0] = cllpr.Group
+	for i, id := range cllpr.LingletProperties {
+		qargs[i+1] = id
+	}
+
+	// SELECT properties
+	stmt := `SELECT properties.id, properties.name, lings_properties.value, lings.name FROM lings_properties INNER JOIN properties ON lings_properties.property_id=properties.id INNER JOIN lings ON lings_properties.ling_id=lings.id WHERE lings.group_id = ? AND lings.depth = 1 AND properties.id IN (?` + strings.Repeat(",?", len(cllpr.LingletProperties)-1) + `)`
+	ps, err := db.Query(stmt, qargs...)
+	if err != nil {
+		log.Print("Error preparing database request!")
+		return CrossLingletPropertiesResponse{}, err
+	}
+	defer ps.Close()
+
+	i := 0
+
+	for ps.Next() {
+		var nv NameValuePair
+		var l Ling
+
+		err = ps.Scan(&nv.Id, &nv.Name, &nv.Value, &l.Name)
+		if err != nil {
+			log.Print("Error executing database request!")
+			return CrossLingletPropertiesResponse{}, err
+		}
+
+		if _, ok := pnMap[nv.Name]; !ok {
+			properties[i] = nv.Name
+			pnMap[nv.Name] = 1
+			i++
+		}
+
+		if n, ok := lMap[l.Name]; ok {
+			lMap[l.Name] = append(n, nv)
+		} else {
+			ns := make([]NameValuePair, 1, 6)
+			ns[0] = nv
+			lMap[l.Name] = ns
+		}
+	}
+
+	j := 0
+
+	for l, n := range(lMap) {
+		if len(n) == len(cllpr.LingletProperties) {
+			// ew
+			s := (*reflect.SliceHeader)(unsafe.Pointer(&n))
+			nArr := *(*[6]NameValuePair)(unsafe.Pointer(s.Data))
+
+			if ls, ok := pMap[nArr]; ok {
+				pMap[nArr] = append(ls, l)
+			} else {
+				pMap[nArr] = []string{l}
+				j++
+			}
+		}
+	}
+
+	pc := make([]CrossLingletPropertiesResponsePropertyCombinations, j)
+
+	k := 0
+
+	for n, ls := range(pMap) {
+		ns := make([]NameValuePair, len(cllpr.LingletProperties))
+		copy(ns, n[:len(cllpr.LingletProperties)])
+		pc[k] = CrossLingletPropertiesResponsePropertyCombinations{
+			PropertyValuePairs: ns,
+			Linglets: ls,
+		}
+
+		k++
+	}
+
+	return CrossLingletPropertiesResponse{
+		Type:     "cross",
+		On:       "linglet_properties",
+		LingletProperties:    properties,
+		PropertyCombinations: pc,
+	}, nil
+}
